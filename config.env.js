3d256fb5d722d921c712528f1c759eb00b63e597
cwash686-png <cwash686@gmail.com>
2026-06-14 20:07:58 +0300

    Upload config.env.js via API

---
+++ config.env.js
@@ -0,0 +1,11 @@
+// WashGo Environment Variables Configuration
+// Copy this template to a file that is not committed to production source control
+window.ENV = {
+  // Firebase configurations (replace with actual production values)
+  FIREBASE_API_KEY: "AIzaSyBNMK4bkwMcYjJzf8nruMtmn8hZDqeZG9g",
+  FIREBASE_AUTH_DOMAIN: "washgo-app-2d669.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "washgo-app-2d669",
+  FIREBASE_STORAGE_BUCKET: "washgo-app-2d669.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "1064703566526",
+  FIREBASE_APP_ID: "1:1064703566526:web:493c7d1c78d360e115f4ef"
+};
